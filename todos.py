6e50e3f7eeff31e36d42f34f5cd5bc942fae6514
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-03-16 15:01:24 +0100

    First tests

---
+++ todos.py
@@ -0,0 +1,10 @@
+class TaskManager:
+    def parse(self, command):
+        return Action("add", command[2:])
+
+    
+class Action:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
