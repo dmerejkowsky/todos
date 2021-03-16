29242474330f82c93d198595bceb7bb409e54a52
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-03-16 14:56:57 +0100

    Fix syntax error

--- test_todos.py
+++ test_todos.py
@@ -1,21 +1,24 @@
-from todos import TaskManager
+from todos import TaskManager, Action
 
 
 def test_can_create_an_action():
     action = Action("add", "first task")
     assert action.name == "add"
-    assert action.description = "first task")
+    assert action.description == "first task"
 
 
 def test_can_create_a_task_manager():
     task_manager = TaskManager()
 
 
+
+
+
 def test_parse_add():
     command = "+ first task"
     task_manager = TaskManager()
-    
+
     action = task_manager.parse(command)
-    
+
     assert action.name == "add"
     assert action.description == "first task"
